8e88f48eb80b5d2f01fba4ce7e72413a7fd470ae
André <AndreR@ppedv.de>
2022-10-10 14:41:04 +0200

    BuilderPattern

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,24 @@
+﻿using HalloBuilder;
+using System.Text;
+
+Console.WriteLine("Hello Builder");
+
+var schrank1 = new Schrank.Builder()
+                          .SetTüren(7)
+                          .SetBöden(4)
+                          .SetOberfläche(Oberfläche.Lackiert)
+                          .SetFarbe("Gelb")
+                          .Create();
+
+
+var schrank2 = new Schrank.Builder()
+                          .SetTüren(2)
+                          .SetBöden(4)
+                          .SetOberfläche(Oberfläche.Lackiert)
+                          .SetFarbe("Gelb")
+                          .Create();
+
+StringBuilder sb = new StringBuilder();
+sb.AppendLine("Hallo");
+sb.AppendLine("Welt");
+Console.WriteLine(sb.ToString());
---
+++ HalloBuilder/HalloBuilder/Schrank.cs
@@ -0,0 +1,71 @@
+﻿namespace HalloBuilder
+{
+    internal class Schrank
+    {
+        public int AnzTüren { get; private set; } = 5;
+        public int AnzBöden { get; private set; } = 5;
+        public string Farbe { get; private set; } = string.Empty;
+        public Oberfläche Oberfläche { get; private set; } = Oberfläche.Natur;
+        public bool Metallschienen { get; private set; } = false;
+        public bool Kleiderstange { get; private set; } = false;
+
+        private Schrank()
+        { }
+
+        internal class Builder
+        {
+            private readonly Schrank _schrank = new();
+
+            public Builder SetTüren(int anzTüren)
+            {
+                if (anzTüren < 2 || anzTüren > 7)
+                    throw new ArgumentException("Zuviele oder zu wenige Türen");
+
+                _schrank.AnzTüren = anzTüren;
+
+                return this;
+            }
+
+            public Builder SetBöden(int anzBöden)
+            {
+                if (anzBöden < 0 || anzBöden > 6)
+                    throw new ArgumentException("Zuviele oder zu wenige Böden");
+
+                _schrank.AnzBöden = anzBöden;
+
+                return this;
+            }
+
+            public Builder SetFarbe(string farbe)
+            {
+                if (string.IsNullOrWhiteSpace(farbe))
+                    throw new ArgumentException("Eine Farbe muss angegeben werden");
+                if (_schrank.Oberfläche != Oberfläche.Lackiert)
+                    throw new ArgumentException("Eine Farbe kann nur gewählt werden, wenn der Schrank lackiert wird");
+
+                _schrank.Farbe = farbe;
+
+                return this;
+            }
+
+            public Builder SetOberfläche(Oberfläche oberfläche)
+            {
+                _schrank.Oberfläche = oberfläche;
+
+                return this;
+            }
+
+            public Schrank Create()
+            {
+                return _schrank;
+            }
+        }
+    }
+
+    enum Oberfläche
+    {
+        Natur,
+        Gewachst,
+        Lackiert
+    }
+}
