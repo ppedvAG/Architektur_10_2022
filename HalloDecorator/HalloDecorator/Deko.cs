3424726da4c75bb742aa397fa7db453b98970c88
André <AndreR@ppedv.de>
2022-10-10 15:11:16 +0200

    Deko Pattern

---
+++ HalloDecorator/HalloDecorator/Deko.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloDecorator
+{
+    internal interface IComponent
+    {
+        string Beschreibung { get; }
+        decimal Preis { get; }
+    }
+
+    class Pizza : IComponent
+    {
+        public string Beschreibung => "Pizza";
+
+        public decimal Preis => 6m;
+    }
+
+    class Brot : IComponent
+    {
+        public string Beschreibung => "Brot";
+
+        public decimal Preis => 4m;
+    }
+
+    abstract class Dekorator : IComponent
+    {
+        protected IComponent parent;
+
+        protected Dekorator(IComponent parent)
+        {
+            this.parent = parent;
+        }
+
+        public abstract string Beschreibung { get; }
+
+        public abstract decimal Preis { get; }
+    }
+
+    class Käse : Dekorator
+    {
+        public Käse(IComponent parent) : base(parent)
+        { }
+
+        public override string Beschreibung => parent.Beschreibung + " Käse";
+
+        public override decimal Preis => parent.Preis + 1.5m;
+    }
+
+    class Salami : Dekorator
+    {
+        public Salami(IComponent parent) : base(parent)
+        { }
+
+        public override string Beschreibung => parent.Beschreibung + " Salami";
+
+        public override decimal Preis => parent.Preis + 3.7m;
+    }
+}
---
+++ HalloDecorator/HalloDecorator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloDecorator;
+
+Console.WriteLine("Hello, World!");
+
+var pizza = new Pizza();
+Console.WriteLine($"{pizza.Beschreibung} {pizza.Preis}");
+
+var brot = new Käse(new Käse(new Käse(new Brot())));
+Console.WriteLine($"{brot.Beschreibung} {brot.Preis}");
+
+
