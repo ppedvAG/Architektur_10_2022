991e896df3fdb4064ea3751c0b2503d73166b50c
André <AndreR@ppedv.de>
2022-10-11 16:30:22 +0200

    ProductManager Fun

--- ppedv.MegaShop5024/ppedv.MegaShop5024.Application.ProductService/ProductManager.cs
+++ ppedv.MegaShop5024/ppedv.MegaShop5024.Application.ProductService/ProductManager.cs
@@ -1,9 +1,23 @@
 ﻿using ppedv.MegaShop5024.Model;
+using ppedv.MegaShop5024.Model.Contracts;
+using ppedv.MegaShop5024.Model.DomainModel;
 
 namespace ppedv.MegaShop5024.Application.ProductService
 {
     public class ProductManager
     {
+        internal IRepository Repository { get; }
 
+        public ProductManager(IRepository repository)
+        {
+            Repository = repository;
+        }
+
+        public Product GetBestWeightPriceProduct()
+        {
+            return Repository.GetAll<Product>()
+                             .OrderBy(x => ((decimal)x.Weight / x.Price))
+                             .FirstOrDefault();
+        }
     }
 }
