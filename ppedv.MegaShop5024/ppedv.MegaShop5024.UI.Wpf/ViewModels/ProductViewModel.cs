b1d4df2ae9d128b391c5e439c61cffb6b8d1ca72
André <AndreR@ppedv.de>
2022-10-12 14:20:00 +0200

    Basic Binding

---
+++ ppedv.MegaShop5024/ppedv.MegaShop5024.UI.Wpf/ViewModels/ProductViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ppedv.MegaShop5024.Model.Contracts;
+using ppedv.MegaShop5024.Model.DomainModel;
+using System;
+using System.Collections.Generic;
+
+namespace ppedv.MegaShop5024.UI.Wpf.ViewModels
+{
+    public class ProductViewModel
+    {
+        //todo bessere DI
+        private IRepository _repo = new Data.EfCore.EfRepository();
+
+        public List<Product> ProductList { get; set; } = new List<Product>();
+
+        public Product SelectedProduct { get; set; }
+
+        public ProductViewModel()
+        {
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            ProductList.Clear();
+            foreach (var item in _repo.Query<Product>())
+            {
+                ProductList.Add(item);
+            }
+        }
+    }
+}
