7c520df1f0431e4ca37e10af97c12dc64e1ceb7e
André <AndreR@ppedv.de>
2022-10-12 10:18:33 +0200

    DI per Relfection

--- ppedv.MegaShop5024/ppedv.MegaShop5024.UI.TestConsole/Program.cs
+++ ppedv.MegaShop5024/ppedv.MegaShop5024.UI.TestConsole/Program.cs
@@ -1,11 +1,22 @@
 ﻿using ppedv.MegaShop5024.Application.ProductService;
 using ppedv.MegaShop5024.Model.Contracts;
 using ppedv.MegaShop5024.Model.DomainModel;
-using System.Security.Cryptography.X509Certificates;
+using System.Reflection;
+
+//DI direkt per Referenz
+//IRepository repo = new ppedv.MegaShop5024.Data.EfCore.EfRepository();
+
+//DI per Reflection
+var path = @"C:\Users\Fred\source\repos\ppedvAG\Architektur_10_2022\ppedv.MegaShop5024\ppedv.MegaShop5024.Data.EfCore\bin\Debug\net6.0\ppedv.MegaShop5024.Data.EfCore.dll";
+var ass = Assembly.LoadFrom(path);
+Type typeMitRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+IRepository repo = Activator.CreateInstance(typeMitRepo) as IRepository;
+
+
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.WriteLine("*** MegaShop 5024 v0.1 ***");
-IRepository repo = new ppedv.MegaShop5024.Data.EfCore.EfRepository();
+
 
 ProductManager pm = new ProductManager(repo);
 var bestProd = pm.GetBestWeightPriceProduct();
