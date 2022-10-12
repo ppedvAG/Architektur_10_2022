4978815590f089e195f12b784530b87d056db75d
André <AndreR@ppedv.de>
2022-10-12 15:06:22 +0200

    ASP MVC

---
+++ ppedv.MegaShop5024/ppedv.MegaShop5024.UI.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ppedv.MegaShop5024.UI.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
